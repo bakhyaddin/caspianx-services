f12ba592e9927a78b1bdf76e27c60b589d51590d
Bahyeddin Nuri <bahyeddin@gmail.com>
2023-04-01 17:03:41 +0300

    feat(user-management): add a new post endpoint for testing

--- user-management/src/app.ts
+++ user-management/src/app.ts
@@ -1,12 +1,21 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   res.send({
     "x-access-token": req.headers["x-access-token"],
     "x-refresh-token": req.headers["x-refresh-token"],
     "x-id-token": req.headers["x-id-token"],
+  });
+});
+
+app.post("/", (req, res) => {
+  console.log({ headers: req.headers });
+  res.send({
+    foo: "bar",
   });
 });
 
